1a7d0e03a63e563a48855367726fb639406dd2d5
omtiwari1803-rgb <omtiwari197707@gmail.com>
2026-03-29 01:32:57 +0530

    Added script4.sh

---
+++ script4.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script 4: Log File Analyzer
+# Author: Om Tiwari
+
+LOGFILE=$1
+KEYWORD=${2:-"error"}
+COUNT=0
+
+# Check if file exists
+if [ ! -f "$LOGFILE" ]; then
+    echo "Error: File not found!"
+    exit 1
+fi
+
+# Read file line by line
+while IFS= read -r LINE
+do
+    if echo "$LINE" | grep -iq "$KEYWORD"; then
+        COUNT=$((COUNT + 1))
+    fi
+done < "$LOGFILE"
+
+echo "----------------------------------"
+echo "Keyword '$KEYWORD' found $COUNT times in $LOGFILE"
+echo "----------------------------------"
+
+echo ""
+echo "Last 5 matching lines:"
+grep -i "$KEYWORD" "$LOGFILE" | tail -5
